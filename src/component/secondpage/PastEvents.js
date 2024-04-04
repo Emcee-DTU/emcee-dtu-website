7ac9dfe83ff4c76939435b479acc5aa474a91cbc
swapnilgarg7 <swapnilgarg810@gmail.com>
2024-04-04 23:02:31 +0530

    changes

--- src/component/secondpage/PastEvents.js
+++ src/component/secondpage/PastEvents.js
@@ -9,19 +9,19 @@ export default function PastEvents() {
     return (
         <div className="past-events-parent">
             <div className="past-events-heading">
-                 <img src={Rectangle_heading1} />
-                 <img src={Rectangle_heading2} />
-                 <h3>Past Events</h3>
+                <img src={Rectangle_heading1} />
+                <img src={Rectangle_heading2} />
+                <h3>Past Events</h3>
             </div>
             <div className="past-events-content">
-                <img src={Rectangle} /> 
+                <img src={Rectangle} />
                 <div className="past-events-part1">
-                    <button id="EngiFest2024">Hosting EngiFest 2024!!</button>
-                    <button id="Yuvaan2024">Hosting Yuvaan 2024!!</button>
-                    <button id="Invictus2024">Hosting Invictus 2024!!</button>
-                    <button id="Freshers2024">Hosting Freshers 2024!!</button>
-                    <button id="E-Cell2024">Hosting E-Cell 2024!!</button>
-                    <button id="EngiFest2023">Hosting Engifest 2023</button>
+                    <button onClick={() => window.open("")} id="EngiFest2024">Hosting EngiFest 2024!!</button>
+                    <button onClick={() => window.open("")} id="Yuvaan2024">Hosting Yuvaan 2024!!</button>
+                    <button onClick={() => window.open("")} id="Invictus2024">Hosting Invictus 2024!!</button>
+                    <button onClick={() => window.open("")} id="Freshers2024">Hosting Freshers 2024!!</button>
+                    <button onClick={() => window.open("")} id="E-Cell2024">Hosting E-Cell 2024!!</button>
+                    <button onClick={() => window.open("")} id="EngiFest2023">Hosting Engifest 2023</button>
                 </div>
             </div>
         </div>
